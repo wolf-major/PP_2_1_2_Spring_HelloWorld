73ce8de5e5028e9e0e9c0f23f55159fb4fb77def
Daria Prudnikova <daria2997@gmail.com>
2025-01-08 15:30:02 +0300

    with Cat class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,11 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld beanTwo =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat catTwo = (Cat) applicationContext.getBean("cat");
+        System.out.println("Is helloWorld's beans equals? - " + (bean == beanTwo));
+        System.out.println("Is cat's beans equals? - " + (cat == catTwo));
     }
 }
